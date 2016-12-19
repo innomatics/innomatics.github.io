77f5b8e4ed3126236b61158e5a726a724efb8cdc
Justin Hopkins <justin@commoncode.com.au>
2016-12-19 20:31:41 +1100

    Added bust to throw score animation

--- dartmate.js
+++ dartmate.js
@@ -1,3 +1,10 @@
+// Parent Divs
+var buttonSection;
+var scoreSection;
+var navSection;
+var optionSection;
+
+var displayMode = 0; // 0 - normal | 1 - options
 
 var scoresRow;
 var numPlayers = 2;
@@ -26,7 +33,7 @@ var players = [
     }
 ];
 var darts = [];
-var infoSection;
+
 var throwScoreSection;
 
 function isDouble(dart)
@@ -62,8 +69,29 @@ function getLabel(dart)
     return getScore(dart);
 }
 
+function toggleOptions()
+{
+  if (displayMode == 0)
+  {
+      // Show options
+      buttonSection.classList.add('hide');
+      scoreSection.classList.add('hide');
+      navSection.classList.add('hide');
+      optionSection.classList.remove('hide');
+  }
+  else
+  {
+    // hide options
+    optionSection.classList.add('hide');
+    buttonSection.classList.remove('hide');
+    scoreSection.classList.remove('hide');
+    navSection.classList.remove('hide');
+  }
+}
+
 
 // Calc scores from the start, every dart
+// Returns last throw score or -1 if busted
 function calculateScores()
 {
     var dartNum = 0;
@@ -72,16 +100,19 @@ function calculateScores()
     var busted = false;
     var throwStartScore;
     var throwStartN;
+    var throwFinished = false;
+
+    // Intitialise player score structures
     for (var p = 0; p < numPlayers; p++)
     {
         players[p].Score = startScore;
         players[p].Darts = [];
-        players[p].n = 0;
+        players[p].n = 0; // Number of darts thrown
     }
 
     for (var t = 0; t < currentDart; t++)
     {
-        dartNum++;
+        dartNum++; // Current player's dart number (1 -3)
 
         // if it's a players first dart, or the last player busted
         // then reset the throw score
@@ -92,7 +123,9 @@ function calculateScores()
             throwStartN = players[currentPlayer].n;
         }
 
-        players[currentPlayer].Score -= getScore(darts[t]);
+        var dartScore = getScore(darts[t]);
+        players[currentPlayer].Score -= dartScore;
+        throwScore += dartScore;
         players[currentPlayer].Darts[players[currentPlayer].n] = darts[t];
         players[currentPlayer].n++;
 
@@ -106,9 +139,8 @@ function calculateScores()
             }
             else
             {
-
                 alert(players[currentPlayer].Name + ' WINS!');
-		return;
+		            return;
             }
         }
 
@@ -119,12 +151,17 @@ function calculateScores()
             players[currentPlayer].n = throwStartN;
         }
 
-        if (dartNum == numDarts || busted)
+        throwFinished = (dartNum == numDarts || busted);
+        if (throwFinished)
         {
             currentPlayer = (currentPlayer + 1) % numPlayers;
             dartNum = 0;
         }
     }
+
+    if (busted) return -1;
+    if (!throwFinished) return null;
+    return throwScore;
 }
 
 
@@ -138,7 +175,19 @@ function addScoreboardButton(parentRow, buttonType, scoreNumber)
         btn.classList.add('scoreboardButton');
         btn.classList.add((buttonType + 'score').toLowerCase());
         parentRow.appendChild(btn);
-	return btn;
+	      return btn;
+}
+
+function addOptionButton(parentRow)
+{
+  var btn = document.createElement('div');
+  btn.Title = 'Options';
+  btn.innerHTML = 'Options';
+  btn.addEventListener('click', function(e){optionButtonClick(e);}, false);
+  btn.classList.add('optionButton');
+  parentRow.appendChild(btn);
+return btn;
+
 }
 
 function updateScores()
@@ -166,12 +215,6 @@ function updateScores()
         }
 
     }
-    infoSection.innerHTML = 'Dart: ' + currentDart + ' ';
-    for (var d = 0; d < currentDart; d++ )
-    {
-        infoSection.innerHTML += (darts[d] + ' ');
-    }
-
 }
 
 function drawThrowScore()
@@ -197,36 +240,40 @@ function hideThrowScore()
 
 function drawScores()
 {
+  scoreSection = document.createElement('div');
+  document.body.appendChild(scoreSection);
+
+
     for (var i = 0; i < numPlayers; i++)
     {
-        var scoreSection = document.createElement('div');
-        scoreSection.classList.add('scoreSection');
-        document.body.appendChild(scoreSection);
-        scoreSection.innerHTML = players[i].Name + ': ' + players[i].Score;
-        players[i].ScoreDiv = scoreSection;
+        var playerScoreSection = document.createElement('div');
+        playerScoreSection.classList.add('scoreSection');
+        scoreSection.appendChild(playerScoreSection);
+        playerScoreSection.innerHTML = players[i].Name + ': ' + players[i].Score;
+        players[i].ScoreDiv = playerScoreSection;
 
         var historySection = document.createElement('div');
         historySection.classList.add('historySection');
         historySection.innerHTML = '-';
-        document.body.appendChild(historySection);
+        scoreSection.appendChild(historySection);
         players[i].HistoryDiv = historySection;
     }
-    infoSection = document.createElement('div');
-    infoSection.classList.add('infoSection');
-    document.body.appendChild(infoSection);
     updateScores();
+
+    navSection = document.createElement('div');
+    document.body.appendChild(navSection);
 
     var backButton = document.createElement('div');
     backButton.classList.add('backButton');
     backButton.addEventListener('click', function(e){goBack(e);}, false);
     backButton.innerHTML = '<< back';
-    document.body.appendChild(backButton);
+    navSection.appendChild(backButton);
 
     var forwardButton = document.createElement('div');
     forwardButton.classList.add('forwardButton');
     forwardButton.addEventListener('click', function(e){goForward(e);}, false);
     forwardButton.innerHTML = 'forward >>';
-    document.body.appendChild(forwardButton);
+    navSection.appendChild(forwardButton);
 
     updateScores();
 
@@ -253,17 +300,25 @@ function goForward()
 }
 
 
+function drawOptions()
+{
+  optionSection = document.createElement('div');
+  optionSection.classList.add('optionSection');
+  document.body.appendChild(optionSection);
+  optionSection.innerHTML = '<h1>Options</h1>';
+}
+
 function drawBoard()
 {
     // Create the scoreboard
     var n = 25;
 
-    var scoreboard = document.createElement('div');
-    scoreboard.classList.add('scoreboard');
-    document.body.appendChild(scoreboard);
+    buttonSection = document.createElement('div');
+    buttonSection.classList.add('scoreboard');
+    document.body.appendChild(buttonSection);
     var sbSpan = document.createElement('div');
     sbSpan.classList.add('scoreboardSpan');
-    scoreboard.appendChild(sbSpan);
+    buttonSection.appendChild(sbSpan);
     while(n > 0)
     {
         var ns = n.toString();
@@ -272,12 +327,12 @@ function drawBoard()
 	{
             sbSpan = document.createElement('div');
             sbSpan.classList.add('scoreboardSpan');
-            scoreboard.appendChild(sbSpan);
+            buttonSection.appendChild(sbSpan);
+            addOptionButton(sbSpan);
 	}
 
         var dbl = addScoreboardButton(sbSpan, 'D', ns);
         var sng = addScoreboardButton(sbSpan, 'S', ns);
-
 
         if (n < 25)
         {
@@ -300,16 +355,22 @@ function drawBoard()
 
     }
 
-    scoresRow = document.createElement('div');
-    scoresRow.Id = 'scoresRow';
-    document.body.appendChild(scoresRow);
+    scoreSection = document.createElement('div');
+    scoreSection.Id = 'scoresRow';
+    document.body.appendChild(scoreSection);
     drawScores();
     drawThrowScore();
+    drawOptions();
 }
 
 function updateScore(player, score)
 {
     scoresRow.innerHTML = 'Score: ' + score;
+}
+
+function optionButtonClick(object)
+{
+  toggleOptions();
 }
 
 // Target click callback
@@ -322,15 +383,17 @@ function scoreboardClick(object)
     }
     darts[currentDart] = dart;
     currentDart ++;
-    calculateScores();
+    var throwScore = calculateScores();
     updateScores();
 
-    if (currentDart > 0 && currentDart % 3 == 0)
+    // Show the score throw if the thow is over
+    if (throwScore)
     {
-      var throwScore = getScore(darts[currentDart -1])
-              + getScore(darts[currentDart - 2])
-              + getScore(darts[currentDart - 3]);
-      displayThrowScore(throwScore);
+      var busted = (throwScore == -1);
+        // var throwScore = getScore(darts[currentDart -1])
+        //         + getScore(darts[currentDart - 2])
+        //         + getScore(darts[currentDart - 3]);
+        displayThrowScore(busted ? 'BUST' : throwScore);
     }
     else
     {
